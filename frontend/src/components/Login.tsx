c51a0cc8421be85ed54f8530debd126be7b57e1c
Patrick Lin <patrick41513@gmail.com>
2024-03-16 13:08:00 +0800

    feat(frontend): setup google login ui

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth, provider } from '../config/firebase';
 import { signInWithPopup } from 'firebase/auth';
-import { Button } from 'primereact/button';
+import { GoogleLoginButton } from "react-social-login-buttons";
+import { AppleLoginButton } from "react-social-login-buttons";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -13,9 +14,13 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Button className="m-3" onClick={login}>Login</Button>
-
+    <div className='w-full grid grid-rows-8 grid-flow-col gap-8 justify-center mt-20'>
+      <GoogleLoginButton onClick={login}>
+        Google Login
+      </GoogleLoginButton>
+      <AppleLoginButton onClick={login}>
+        Apple Login
+      </AppleLoginButton>
     </div>
   );
 };
